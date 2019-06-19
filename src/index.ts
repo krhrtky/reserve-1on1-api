39909ef0193fa96be9ff27c33c856473de53b07a
t-kurihara <takuya.kurihara00@gmail.com>
2019-06-19 23:43:41 +0900

    [add] go to login / open gmail page.

--- src/index.ts
+++ src/index.ts
@@ -11,34 +11,45 @@ const start = async () => {
   try {
     browser = await puppeteer.launch({
       headless: process.env.ENV === 'production'
-      // args: ['--start-fullscreen'],
-      // defaultViewport: {
-      //   width: 1600,
-      //   height: 1000
-      // }
     })
     const page = await browser.newPage()
-    await page.goto('https://www.google.com/?hl=ja')
-    await page.waitForSelector('#gb_70')
-    await page.click('#gb_70')
-    await page.waitForSelector('#initialView')
-    await page.type('#identifierId', user.name)
-    const idNextButton = await page.$('#identifierNext')
-    await idNextButton!.click()
-
-    await page.waitForSelector('input[type="password"]')
-    await page.waitForSelector('#passwordNext')
-    await page.type('#password input[type="password"]', user.password)
-
-    await page.evaluate(
-      (): void => {
-        document.getElementById('#passwordNext')!.click()
+    await page.emulate({
+      userAgent:
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+      viewport: {
+        width: 1524,
+        height: 820,
+        deviceScaleFactor: 1,
+        isMobile: false,
+        hasTouch: false,
+        isLandscape: false
       }
-    )
-    // await page.click('#passwordNext')
-    page.waitForResponse('https://www.google.com/?hl=ja')
+    })
 
-    await page.screenshot({ path: 'next.png' })
+    await page.goto('https://accounts.google.com/signin/v2/identifier')
+    await page.waitForSelector('#identifierId')
+
+    await page.type('#identifierId', user.name)
+    await page.click('#identifierNext')
+
+    await page.waitForSelector('#password input[type="password"]', {
+      visible: true
+    })
+    await page.waitFor(2000)
+
+    const result = await page.evaluate((password: string): string => {
+      const passwordFrom = document.querySelector(
+        'input[type="password"]'
+      ) as HTMLInputElement
+      passwordFrom.value = password
+      return passwordFrom.value
+    }, user.password)
+
+    await page.click('#passwordNext')
+    await page.waitFor(2000)
+
+    await page.goto('https://mail.google.com/mail/u/0/?tab=wm&ogbl#inbox')
+    await page.waitFor(1000)
   } catch (e) {
     console.error(e)
   } finally {
