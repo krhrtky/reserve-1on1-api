f0f7dcb86b6a18715616c3ad51f52bb4a7ffed01
t-kurihara <takuya.kurihara00@gmail.com>
2019-06-24 23:41:17 +0900

    [add] send mail process.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import puppeteer, { Browser } from 'puppeteer'
-import { User } from './types'
+import { MailTemplate, User } from './types'
 import { login } from './login'
 import { send } from './send'
 
 const user: User = {
   name: process.env.USER_NAME as string,
   password: process.env.PASSWORD as string
+}
+
+const template: MailTemplate = {
+  to: process.env.TO as string,
+  subject: process.env.SUBJECT as string,
+  body: process.env.BODY as string
 }
 
 const start = async () => {
@@ -17,7 +23,7 @@ const start = async () => {
     const page = await browser.newPage()
 
     await login(page, user)
-    await send(page, { to: 'to', body: 'body', subject: 'subject' })
+    await send(page, template)
   } catch (e) {
     console.error(e)
   } finally {
--- src/send.ts
+++ src/send.ts
@@ -8,6 +8,15 @@ export const send: (
   page.goto('https://mail.google.com/mail/u/0/?tab=wm&ogbl#inbox', {
     waitUntil: 'domcontentloaded'
   })
+  await page.waitForSelector('div[role="button"][gh="cm"]')
   await page.click('div[role="button"][gh="cm"]')
-  await page.waitForSelector('#\\:p4')
+
+  await page.waitForSelector('textarea[name="to"]')
+  await page.type('textarea[name="to"]', template.to)
+  await page.type('input[name="subjectbox"]', template.subject)
+  await page.type('div[role="textbox"]', template.body)
+
+  await page.click('div[role="button"][data-tooltip^="送信"]')
+  await page.screenshot({ path: 'send.png' })
+  await page.waitFor(2000)
 }
